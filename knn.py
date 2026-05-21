725ba5b22fea3e1b237f5339f7185794e5710bbb
Farah <farah@email.com>
2026-05-21 09:27:26 +0200

    optimisation du KNN

---
+++ knn.py
@@ -0,0 +1,15 @@
+class KNN :
+    def __init__(self, k):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        distances = []  
+    
+        for neighbor_index, point in enumerate(self.X_train):
+         distances.append((((X - point)**2)**0.5, neighbor_index))
+        distances.sort()
+        k_nearest = distances[:self.k]
